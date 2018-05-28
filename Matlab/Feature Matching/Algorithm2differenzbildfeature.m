a01beba08239bd53b690edaf8f61dc4dab84f9a9
cxy842723494 <cxy842723494@gmail.com>
2018-05-28 16:28:36 +0200

    2805-2

--- Matlab/Feature Matching/Algorithm2differenzbildfeature.m
+++ Matlab/Feature Matching/Algorithm2differenzbildfeature.m
@@ -22,9 +22,27 @@ clean;
                 image_name2 = img_path_list(j).name;    %  image2  
                 [yuv(2).Y,yuv(2).U,yuv(2).V] =  readYUV(strcat(file_path,image_name2));  
                 
-                I1 = yuv(1).Y/255; figure, histogram(I1);
-                I2 = yuv(2).Y/255; figure, histogram(I2);
+                I1 = imresize(yuv(1).Y,[1080 1920]);
+                I1 = I1/255; figure, histogram(I1);
+                I2 = imresize(yuv(2).Y,[1080 1920]);
+                I2 = I2/255; figure, histogram(I2);
+               
                 MOVINGREG = registerImages(I1,I2);
+                
+                figure,imshow(MOVINGREG.RegisteredImage);
+                tformY = MOVINGREG.Transformation;  
+                
+                U_teil1new = imwarp(yuv(1).U, tformY,'OutputView', imref2d(size(yuv(1).U)));
+                diffc_U = U_teil1new - yuv(2).U;
+                figure,imshow(abs(diffc_U),[]),title('after transform data diffc in U');figure,histogram(diffc_U)
+                E = imcrop(abs(diffc_U),[11 11 1899 1059]);
+                figure,imshow(E,[]),figure, histogram(E);meansqr(E);
+                figure,imshow(diffc_U,[]),title('after transform data diffc in U');figure,histogram(diffc_U)
+                figure,imshow(abs(yuv(1).U- yuv(2).U),[]);
+                
+                
+                
+                % weiter
                 
                 
                 points1 = detectSURFFeatures(I1);
@@ -35,6 +53,8 @@ clean;
                 tform = MOVINGREG.Transformation;
                 Yw(:,:,k) = imwarp(Y(:,:,k), tformY, 'OutputView', imref2d(size(Y(:,:,k))));
                 estimateGeometricTransform
+                
+                energie = meansqr(diffc_U);
                 
                 matchedPoints1 = vpts1(indexPairs(:,1));Transformation
                 matchedPoints2 = vpts2(indexPairs(:,2));
