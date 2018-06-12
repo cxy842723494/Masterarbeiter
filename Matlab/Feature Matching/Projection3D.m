497ea4996de3f5e0671a65ea7f284d707af6deaf
cxy842723494 <cxy842723494@gmail.com>
2018-06-12 16:30:54 +0200

    1106

---
+++ Matlab/Feature Matching/Projection3D.m
@@ -0,0 +1,60 @@
+function J = Projection3D(x,y,Img,f,delta11,delta12,delta13,delta21,delta22,delta23)
+%   Input: the feature point come from feature detection an RANSAC to
+%   discard outliers. 
+%   x,y: corresponding feature image point, X,Y: corresponding point in 3D world coordinates
+
+%   Parameter: focal length f; Rotation angle: delta11,delta12,delta13,delta21,delta22,delta23
+
+%     Img= imread('IMG_0353.jpg');
+    vw = size(Img,2);
+    vh = size(Img,1);
+%     f=100;
+    
+% Instrinsic camera matrix
+    K1 = [1 0 -vw/2; 0 1 -vh/2; 0 0 f];
+    K = inv(K1);
+%     X = K1*[x(i,:)'; 1];
+%     Y = K1*[y(i,:)'; 1];
+
+% Orientation R(t)
+    Rx1 = [1            0           0;
+          0 cos(delta11) -sin(delta11);
+          0 sin(delta11)  cos(delta11)];
+
+    Ry1 = [ cos(delta12) 0 sin(delta12);
+          0           1           0;
+          -sin(delta12) 0 cos(delta12)];
+
+    Rz1 = [cos(delta13) -sin(delta13) 0;
+          sin(delta13)  cos(delta13) 0;
+          0           0            1];
+      
+    Rx2 = [1            0           0;
+          0 cos(delta21) -sin(delta21);
+          0 sin(delta21)  cos(delta21)];
+
+    Ry2 = [ cos(delta22) 0 sin(delta22);
+          0           1           0;
+          -sin(delta22) 0 cos(delta22)];
+
+    Rz2 = [cos(delta23) -sin(delta23) 0;
+          sin(delta23)  cos(delta23) 0;
+          0           0            1];
+      
+    R1 = Rz1 * Ry1 * Rx1;  
+    R2 = Rz2 * Ry2 * Rx2; 
+    
+%  corresponding feature image point   
+    W = K*R1*R2.'*K1;
+    
+%     y_cor = W*x;
+    
+% minimize the mean-squareed-projection error
+    J = sum(sum((y - W*x).^2));
+    
+    
+      
+      I1 = imread('IMG_0356.JPG');
+      I2 = imread('IMG_0357.JPG');
+      figure, imshowpair(I1,I2);
+      figure, imshow(I1);
--- Matlab/Feature Matching/VerificationFor2DRansac.m
+++ Matlab/Feature Matching/VerificationFor2DRansac.m
@@ -1,14 +1,14 @@
 clean;
 
-Img1= imread('IMG_0351.jpg');
-Img2= imread('IMG_0352.jpg');
+Img1= imread('IMG_0363.jpg');
+Img2= imread('IMG_0364.jpg');
 
 Igray1 = rgb2gray(Img1);
 Igray2 = rgb2gray(Img2);
 figure, imshow(Igray1);
 figure, imshow(Igray2);
 
-MOVINGREG = registerImagestext1(Igray1,Igray2);
+% MOVINGREG = registerImagestext1(Igray1,Igray2);
 points1 = detectSURFFeatures(Igray1);
 points2 = detectSURFFeatures(Igray2);
 
@@ -22,10 +22,11 @@ matchedPoints2 = valid_points2(indexPairs(:,2),:);
 figure; showMatchedFeatures(Igray1,Igray2,matchedPoints1,matchedPoints2);title('Candidate matched points (including outliers)');
 
 [tform,inlierpoints1,inlierpoints2] = estimateGeometricTransform(...
-matchedPoints1,matchedPoints2,'projective','MaxDistance',1.5);
+matchedPoints1,matchedPoints2,'projective');
 figure; showMatchedFeatures(Igray1,Igray2,inlierpoints1,inlierpoints2);
 title('Matched inlier points');
-  
+
+%%
  MOVINGREG.Transformation = tform;
  MOVINGREG.RegisteredImage = imwarp(Igray1, imref2d(size(Igray1)), tform, 'OutputView', imref2d(size(Igray2)), 'SmoothEdges', true);
  figure, imshow(MOVINGREG.RegisteredImage);
