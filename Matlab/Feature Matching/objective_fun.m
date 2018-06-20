081baf3cc79e1cc8da98367573cd3222dd6afc27
cxy842723494 <cxy842723494@gmail.com>
2018-06-21 00:30:33 +0200

    2006end

--- Matlab/Feature Matching/objective_fun.m
+++ Matlab/Feature Matching/objective_fun.m
@@ -1,4 +1,4 @@
-% p0: initial parameter: f 45°,thetaz,thetay,thetax 0
+% p0: initial parameter: f 45?,thetaz,thetay,thetax 0
 % dp: small differenz, same size with p0
 % term_dp: terminate condition
 % x1,x2: the corresponding point
@@ -8,23 +8,30 @@ function [J] = objective_fun(p0,x1,x2,frame_size) % frame_time
 w=size(frame_size,2);
 h=size(frame_size,1);
 p = p0;
-pX1 = getW(p.f,p.thetaz,p.thetay,p.thetax,w,h)*x2 ;
-dxy = x1(1:2,:) - ( Vec(pX|X, pX|Y) / (pX|Z) );
+pX1 = getW(p.thetaz,p.thetay,p.thetax,w,h)*x2 ;    % p.f,
+for i =1:size(pX1,2)
+    pX1(1,i) = pX1(1,i)/ pX1(3,i);
+    pX1(2,i) = pX1(2,i)/ pX1(3,i);
+end
+dxy = x1(1:2,:) - pX1(1:2,:) ;
+J = sqrt(sum(dot(dxy,dxy)));
 
 end
 
 
-function [W] = getW(f,thetaz,thetay,thetax,w,h) 
+function [W] = getW(thetaz,thetay,thetax)           % f,w,h
 % intrinsic camera Matrix
-K = [1, 0, -w/2;
-      0, 1, -h/2;
-      0, 0, f];
-              
-invK = [1, 0, w/(2*f);
-        0, 1, h/(2*f);
-        0, 0, 1/f];
+load('cameraParametersGooglePixel2XL.mat');
+invK = cameraParams.IntrinsicMatrix;
+K = inv(invK);
+% K = [1, 0, -w/2;
+%       0, 1, -h/2;
+%       0, 0, f];
+%               
+% invK = [1, 0, w/(2*f);
+%         0, 1, h/(2*f);
+%         0, 0, 1/f];
     
-
 Rotz = [cos(thetaz)  -sin(thetaz) 0; ...
         sin(thetaz)  cos(thetaz)  0; ...
         0            0            1]; 
