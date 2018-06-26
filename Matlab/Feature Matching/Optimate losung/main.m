004606474517e288aecce47824be780b9316eeb4
cxy842723494 <cxy842723494@gmail.com>
2018-06-26 17:40:36 +0200

    260601

--- Matlab/Feature Matching/Optimate losung/main.m
+++ Matlab/Feature Matching/Optimate losung/main.m
@@ -2,79 +2,63 @@ clean;
 % parameter: f,thetaz,thetay,thetax,ts,td,wd
 % input: 2 set of corresponding points after sift und RANSAC x,y
 
-% load the image
-fn1 ='YUV_2018_06_20_17_08_45_933.yuv';
-fn2 ='YUV_2018_06_20_17_08_45_919.yuv';
+%% load the image
+% fn1 ='YUV_2018_06_20_17_08_45_933.yuv';
+% fn2 ='YUV_2018_06_20_17_08_45_919.yuv';
+fn1 ='YUV_2018_06_26_17_27_29_610.yuv';
+fn2 ='YUV_2018_06_26_17_26_59_679.yuv';
 [yuv(1).Y,yuv(1).U,yuv(1).V]= readYUV(fn1);
 [yuv(2).Y,yuv(2).U,yuv(2).V]= readYUV(fn2);
 
-
 Igray1 = yuv(1).Y/255;
 Igray2 = yuv(2).Y/255;
+frame_size = size(Igray1);
 % figure, imshow(Igray1);
 % figure, imshow(Igray2);
-% feature detection
-points1 = detectSURFFeatures(Igray1);
-points2 = detectSURFFeatures(Igray2);
 
-[features1, valid_points1] = extractFeatures(Igray1, points1);
-[features2, valid_points2] = extractFeatures(Igray2, points2);
-% match the point
-indexPairs = matchFeatures(features1,features2);
+%% feature detection
+[pts1h,pts2h] = matchedinlierspoint(Igray1,Igray2);
 
-matchedPoints1 = valid_points1(indexPairs(:,1),:);
-matchedPoints2 = valid_points2(indexPairs(:,2),:);
-% figure; showMatchedFeatures(Igray1,Igray2,matchedPoints1,matchedPoints2);title('Candidate matched points (including outliers)');
+%% Algorithmus
+State = 2;              % 0:test nur Rotation 
+                        % 1:test Rotation and Focal length
+                        % 2:test Rotation, Translation and Focal length
+                        % 3:weiter                       
+switch State
+    case 0        
+        [p0,J] = J_Rotation(pts1h, pts2h,State,frame_size);
+    case 1
+        [p0,J] = J_Rotation_focal(pts1h,pts2h,State,frame_size);
+    case 2
+        [p0,J] = J_Rotation_Translation(pts1h, pts2h,State,frame_size);
+    case 3
+               
+end
+%% Auswert
+auswert(p0,pts1h,pts2h,frame_size,State,Igray1) 
 
-% discard outliers with RANSAC
-[fLMedS, inliers] = estimateFundamentalMatrix(matchedPoints1,matchedPoints2,'Method','RANSAC','DistanceThreshold',0.1);
-inlierPts1 = matchedPoints1(inliers,:);
-inlierPts2 = matchedPoints2(inliers,:);
+    
+%% P0 parameter include Rotation 
+function [p0,J] = J_Rotation(pts1h, pts2h, State,frame_size)
+p0=[0 0 0];
+dp=[0.1 0.1 0.1 ]; % mit 0.1 grad verändern 
+term_dp=[0.001 0.001 0.001 ]; % threshold 0.001 grad
+[p0,J] = J_optimizieren(p0, dp, term_dp, pts1h, pts2h, frame_size,State);
+end
 
-figure;
-showMatchedFeatures(Igray1, Igray2, inlierPts1,inlierPts2,'montage','PlotOptions',{'ro','go','y--'});
-title('Point matches after outliers were removed');
+%% P0 parameter include Rotation and Focal length: 
+function [p0,J] = J_Rotation_focal(pts1h, pts2h, State,frame_size)
+p0=[0 0 0 3200];  % 3200 or -3200
+dp=[0.1 0.1 0.1 10]; % mit 0.1 grad verändern and focal length mit 1 pixel
+term_dp=[0.001 0.001 0.001 1]; % 
+[p0,J] = J_optimizieren(p0, dp, term_dp, pts1h, pts2h, frame_size, State);
+end
 
-integerClass = 'int32';
-outputClass = 'double';
+%% P0 parameter include Rotation Translation und Focal length:
+function [p0,J] = J_Rotation_Translation(pts1h, pts2h, State,frame_size)
+p0=[0 10 0 100 0 0];  % first 3 Rotation others Translation
+dp=[0.1 0.1 0.1 1 1 1]; % % mit 0.1 grad verändern and 0.1 mm Translation
+term_dp=[0.001 0.001 0.001 0.001 0.001 0.001]; % 
+[p0,J] = J_optimizieren(p0, dp, term_dp, pts1h, pts2h, frame_size, State);
+end
 
-nPts = cast(size(inlierPts1, 1), integerClass);
-
-pts1h = coder.nullcopy(zeros(3, nPts, outputClass));
-pts2h = coder.nullcopy(zeros(3, nPts, outputClass));
-
-
-pts1h(3, :)   = 1;
-pts2h(3, :)   = 1;
-
-pts1h(1:2, :) = inlierPts1.Location';
-pts2h(1:2, :) = inlierPts2.Location';
-
-%% p0 parameter nur theta: 
-% p0=[0.5 0.5 0.5];
-% dp=[0.01 0.01 0.01 ]; % 0,11 grad
-% term_dp=[0.001 0.001 0.001 ]; % 0.005 grad
-% frame_size = size(Igray1);
-% [p0,J] = J_optimizieren(p0, dp, term_dp, pts1h, pts2h, frame_size);
-% x=1; 
-
-%% p0 parameter include theta and focal length: 
-% p0=[-1 1 1 3180];  %-1500  3180
-% dp=[0.1 0.1 0.1 1]; % 0,11 grad
-% term_dp=[0.001 0.001 0.001 0.2]; % 0.005 grad
-% frame_size = size(Igray1);
-% [p0,J] = J_optimizieren(p0, dp, term_dp, pts1h, pts2h, frame_size);
-% x=1;
-
-%% p0 parameter include theta shift and focal length: 
-p0=[0 0 0 0 0 0];  %-1500  3180
-dp=[0.1 0.1 0.1 1 1 1]; % 0,11 grad
-term_dp=[0.0001 0.0001 0.0001 0.0001 0.0001 0.0001]; % 0.005 grad
-frame_size = size(Igray1);
-[p0,J] = J_optimizieren(p0, dp, term_dp, pts1h, pts2h, frame_size);
-x=1;
-
-
-% err += sqrt(dot(dxy, dxy));
-% 
-% err /= num_pts;
--- Matlab/Feature Matching/Optimate losung/objective_fun.m
+++ Matlab/Feature Matching/Optimate losung/objective_fun.m
@@ -1,98 +1,139 @@
-% p0: initial parameter: f 45?,thetaz,thetay,thetax 0
+% P0: initial parameter:
+% thetaz,thetay,thetax,focal length,Translation_z,Translation_y,Translation_x
 % dp: small differenz, same size with p0
 % term_dp: terminate condition
 % x1,x2: the corresponding point
 % frame_time
-% 
-function [J] = objective_fun(p0,x1,x2,frame_size) % frame_time
+
+function [J,x1,x2,pX1,dxy] = objective_fun(p0,x1,x2,frame_size,State) % frame_time
 w=frame_size(2);
 h=frame_size(1);
-p = p0; % p =[0.01 0 0];p =[0 0.01 0 ];p =[0 0 0.002 0.025];
-% pX1 = getW(p(1),p(2),p(3))*x2 ; 
+P = p0; 
+% p =[0.01 0 0];p =[0 0.01 0 ];p =[0 0 0.002 0.025];
 
-% W = getW_mit_f(p(1),p(2),p(3),p(4),w,h);
-% pX1 = getW_mit_f(p(1),p(2),p(3),p(4),w,h)*x2 ;    % p.f,
- x1(4, :)   = 1;
- x2(4, :)   = 1;
- pX1 = getW_shift(p(1),p(2),p(3),p(4),p(5),p(6))*x2 ;  
+% Switch
+% 0:test nur Rotation 
+% 1:test Rotation and Focal length
+% 2:test Rotation, Translation and Focal length
+% 3:weiter
+                        
+switch State 
+    case 0
+        W = getW_Rotation(P(1),P(2),P(3));        
+    case 1
+        W = getW_Rotation_F2(P(1),P(2),P(3),P(4),w,h);        
+    case 2
+        x1(4, :) = 1;
+        x2(4, :) = 1;
+        W = getW_Rotation_Translation(P(1),P(2),P(3),P(4),P(5),P(6),w,h);         
+end
 
+pX1 = W*x2 ; 
 for i =1:size(pX1,2)
     pX1(1,i) = pX1(1,i)/ pX1(3,i);
     pX1(2,i) = pX1(2,i)/ pX1(3,i);
 end
 dxy = x1(1:2,:) - pX1(1:2,:) ;
 
-
 J = sum(sqrt(dot(dxy,dxy)));
 
 end
 
 
-function [W] = getW_mit_f(thetaz,thetay,thetax,f,w,h)           % f,w,h
+function [W] = getW_Rotation(thetaz,thetay,thetax)           % f,w,h
 % intrinsic camera Matrix
-K = [ 1, 0, -w/2;
-      0, 1, -h/2;       % k.^-1
-      0, 0, f];
-              
-invK = [1, 0, w/(2*f);
-        0, 1, h/(2*f);  % parameter lambda 
-        0, 0, 1/f];
-    % K*inv(K)=I;
-
-% Rotz = [cosd(thetaz)  -sind(thetaz) 0; ...
-%         sind(thetaz)  cosd(thetaz)  0; ...
-%         0            0            1];  
-%  thetaz =10;   thetax =20; thetay =30; 
-Rotz = [cosd(-thetaz)  -sind(-thetaz) 0; ...   % -thetaz?
-        sind(-thetaz)  cosd(-thetaz)  0; ...
-        0             0            1];  
-Roty = [cosd(thetay)  0            sind(thetay) ; ...
-        0             1            0 ; ...
-        -sind(thetay) 0            cosd(thetay)]; 
-Rotx = [1             0            0 ; ...
+load('cameraParametersGooglePixel2XL.mat');
+K = cameraParams.IntrinsicMatrix.';
+invK = inv(K);
+norm_invK = invK/3.142160374450406e-04;   % 1 0 -ox
+                                          % 0 1 -oy
+                                          % 0 0 f  
+%  thetaz =0;   thetay = 0; thetax =0; 
+Rotz = [cosd(thetaz)  -sind(thetaz) 0; ...
+        sind(thetaz)  cosd(thetaz)  0; ...
+        0             0             1];  
+Roty = [cosd(thetay)  0             sind(thetay) ; ...
+        0             1             0 ; ...
+        -sind(thetay) 0             cosd(thetay)]; 
+Rotx = [1             0             0 ; ...
         0             cosd(thetax)  -sind(thetax); ...
         0             sind(thetax)  cosd(thetax)]; 
 % R = Rotz * Roty * Rotx;
 
-W = invK * Rotz * Roty * Rotx * K;
-
+W = K * Rotz * Roty * Rotx * invK;
 end
 
-function [W] = getW(thetaz,thetay,thetax)           % f,w,h
+function [W] = getW_Rotation_F(thetaz,thetay,thetax,f,w,h)           % f,w,h
 % intrinsic camera Matrix
-load('cameraParametersGooglePixel2XL.mat');
-invK = cameraParams.IntrinsicMatrix.';
+invK = [ 1, 0, -w/2;
+         0, 1, -h/2;       
+         0, 0, f];
+              
+K = [1, 0, w/(2*f);
+     0, 1, h/(2*f);  
+     0, 0, 1/f];
+% K*inv(K)=I;
 
-K = inv(invK)/3.142160374450406e-04;
-
-Rotz = [cosd(thetaz)  -sind(thetaz) 0; ...
+% thetaz =10;   thetax =20; thetay =30; 
+Rotz = [cosd(thetaz)  -sind(thetaz) 0; ...  
         sind(thetaz)  cosd(thetaz)  0; ...
-        0            0            1];  
-%  thetaz =0.01;   thetay = 0; thetax =0; 
-% Rotz = [cosd(-thetaz)  -sind(-thetaz) 0; ...   % -thetaz?
-%         sind(-thetaz)  cosd(-thetaz)  0; ...
-%         0            0            1];  
-Roty = [cosd(thetay)  0            sind(thetay) ; ...
-        0            1            0 ; ...
-        -sind(thetay) 0            cosd(thetay)]; 
-Rotx = [1            0            0 ; ...
-        0            cosd(thetax)  -sind(thetax); ...
-        0            sind(thetax)  cosd(thetax)]; 
+        0             0             1];  
+Roty = [cosd(thetay)  0             sind(thetay) ; ...
+        0             1             0 ; ...
+        -sind(thetay) 0             cosd(thetay)]; 
+Rotx = [1             0             0 ; ...
+        0             cosd(thetax)  -sind(thetax); ...
+        0             sind(thetax)  cosd(thetax)]; 
 % R = Rotz * Roty * Rotx;
 
-W = invK * Rotz * Roty * Rotx * K;
+W = K * Rotz * Roty * Rotx * invK;
+
 end
 
-function [W] = getW_shift(thetaz,thetay,thetax,tx,ty,tz)           % f,w,h
-T = [tx,ty,tz];
+function [W] = getW_Rotation_F2(thetaz,thetay,thetax,f,w,h)           % f,w,h
 % intrinsic camera Matrix
-load('cameraParametersGooglePixel2XL.mat');
-invK = cameraParams.IntrinsicMatrix;
-invKn = [invK; 0 0 0];invKnn(1:3,:) = invKn';
-invKnn(4,:)=[0 0 0 1]; 
- K = invK'; K =inv(K)/3.142160374450406e-04;K =[K';0 0 0 ];
-K = [K';0 0 0 1];
-invKnn=invKnn'; 
+K = [f, 0, w/2;
+     0, f, h/2;  
+     0, 0, 1];
+invK = inv(K); 
+invK2 = [ 1, 0, -w/2;  % invK = lamdba * invK2 ; lamdba = 1/f;
+         0, 1, -h/2;       
+         0, 0, f];                   
+% K*inv(K)=I;
+
+% thetaz =10;   thetax =20; thetay =30; 
+Rotz = [cosd(thetaz)  -sind(thetaz) 0; ...  
+        sind(thetaz)  cosd(thetaz)  0; ...
+        0             0             1];  
+Roty = [cosd(thetay)  0             sind(thetay) ; ...
+        0             1             0 ; ...
+        -sind(thetay) 0             cosd(thetay)]; 
+Rotx = [1             0             0 ; ...
+        0             cosd(thetax)  -sind(thetax); ...
+        0             sind(thetax)  cosd(thetax)]; 
+% R = Rotz * Roty * Rotx;
+
+W = K * Rotz * Roty * Rotx * invK;
+
+end
+
+
+function [W] = getW_Rotation_Translation(thetaz,thetay,thetax,tx,ty,tz,w,h)           % f,w,h
+T = [tx,ty,tz];
+f = -3200; 
+% intrinsic camera Matrix
+% load('cameraParametersGooglePixel2XL.mat');
+% invK = cameraParams.IntrinsicMatrix;
+% invKn = [invK; 0 0 0];invKnn(1:3,:) = invKn';
+% invKnn(4,:)=[0 0 0 1]; 
+%  K = invK'; K =inv(K)/3.142160374450406e-04;K =[K';0 0 0 ];
+% K = [K';0 0 0 1];
+% invKnn=invKnn'; 
+K = [f, 0, w/2, 0;
+     0, f, h/2, 0;  
+     0, 0, 1,   0;
+     0, 0, 0,   1];
+invK = inv(K);
 
 Rotz = [cosd(thetaz)  -sind(thetaz) 0; ...
         sind(thetaz)  cosd(thetaz)  0; ...
@@ -107,11 +148,10 @@ Roty = [cosd(thetay)  0            sind(thetay) ; ...
 Rotx = [1            0            0 ; ...
         0            cosd(thetax)  -sind(thetax); ...
         0            sind(thetax)  cosd(thetax)]; 
- R = Rotz * Roty * Rotx;
+R = Rotz * Roty * Rotx;
 RTtemp = [R';T];
 RT(1:3,:) = RTtemp';
 RT(4,:) =[0 0 0 1];
-% Q = [1 0 0 0;0 1 0 0;0 0 1 0];
 
-W = invKnn  * RT * K;
+W = K * RT * invK;
 end
